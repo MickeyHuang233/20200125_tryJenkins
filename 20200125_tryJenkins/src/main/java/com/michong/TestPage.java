bbf722506d9c17182d8795c0d48cc11af02b5686
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2020-01-26 10:32:36 +0800

    update

--- 20200125_tryJenkins/src/main/java/com/michong/TestPage.java
+++ 20200125_tryJenkins/src/main/java/com/michong/TestPage.java
@@ -1,7 +1,7 @@
 package com.michong;
 
 public class TestPage {
-	String pageName = "test jenkins CI page. Test test test...";
+	String pageName = "test jenkins CI page. Test test test... test test";
 
 	public String getPageName() {
 		return pageName;
